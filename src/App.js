ebe01825caa832a56d2fbbe45c07b4f5a5a9aa8b
Mikhailo Marynenko <0x77dev@protonmail.com>
2022-02-02 19:43:13 +0200

    add dbl click handler

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
   const [{ background, fill }, set] = useSpring({ background: '#f0f0f0', fill: '#202020' }, [])
 
   return (
-    <a.main style={{ background }}>
+    <a.main onDoubleClick={() => location.replace('/links')} style={{ background }}>
       <Canvas className="canvas" dpr={[1, 2]}>
         <Scene setBg={set} />
         <OrbitControls enablePan={false} enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
